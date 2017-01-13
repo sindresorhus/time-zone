af7c02fd2e29a3e51a8aca1bb3b4bb4d6bfb84a0
Sindre Sorhus <sindresorhus@gmail.com>
2017-01-13 16:43:08 +0700

    ES2015ify and require Node.js 4

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 'use strict';
-module.exports = function (date) {
-	var offset = (date || new Date()).getTimezoneOffset();
-	var absOffset = Math.abs(offset);
-	var hours = Math.floor(absOffset / 60);
-	var minutes = absOffset % 60;
-	var minutesOut = minutes > 0 ? ':' + ('0' + minutes).slice(-2) : '';
+module.exports = date => {
+	const offset = (date || new Date()).getTimezoneOffset();
+	const absOffset = Math.abs(offset);
+	const hours = Math.floor(absOffset / 60);
+	const minutes = absOffset % 60;
+	const minutesOut = minutes > 0 ? ':' + ('0' + minutes).slice(-2) : '';
 
 	return (offset < 0 ? '+' : '-') + hours + minutesOut;
 };
